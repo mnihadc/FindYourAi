87e45ebf0c5fcd5c25e7f7a0fe0c29af2a2bbf12
Nihad <mncnihad@gmail.com>
2025-01-06 15:15:05 +0530

    created navbar and setup hbs engines

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const exphbs = require("express-handlebars");
+const handlebars = require("handlebars");
+const path = require("path");
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
 const app = express();
 
 dotenv.config();
 
 const port = process.env.PORT_NO;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // Global error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something went wrong!");
 });
+app.engine(
+  "hbs",
+  exphbs.engine({
+    extname: "hbs",
+    defaultLayout: "main",
+    handlebars: allowInsecurePrototypeAccess(handlebars),
+    layoutsDir: path.join(__dirname, "views", "layouts"),
+  })
+);
+
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
